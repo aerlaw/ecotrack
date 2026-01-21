fc959a651ec3a673b016cfc7745346f084226dd7
Aerlaw <jaiunequestion@hotmail.com>
2026-01-21 11:29:04 +0100

    Initial commit Expo ecotrack

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+import { Tabs } from 'expo-router'
+import {MaterialCommunityIcons as Icon} from '@expo/vector-icons'
+
+export default function TabsLayout() { 
+    return (
+      <Tabs>
+        <Tabs.Screen name="index" 
+        options={{ 
+            title: "Home",
+            tabBarLabel: "Acceuil",
+            tabBarIcon: ({ color, size }) => (
+                <Icon name="view-dashboard" color={color} size={size} />
+            )
+         }} />
+        <Tabs.Screen 
+            name="signalement-conteneur" 
+            options={{ 
+                title: "Conteneur Plein",
+                tabBarLabel: "Conteneur",
+                tabBarIcon: ({ color, size }) => (
+                    <Icon name="delete-alert" color={color} size={size} />
+            )
+         }} />
+      </Tabs>
+    )
+}
+
+const styles = StyleSheet.create({})
---
+++ app/(tabs)/singelement-conteneur.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function SignalementConteneur() {
+  return (
+    <View>
+      <Text>signalement-conteneur</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
